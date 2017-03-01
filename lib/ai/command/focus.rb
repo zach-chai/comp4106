2969b5ff09349c829945d947266e7ccf071b4708
Zachary Chai <zachary.chai@carleton.ca>
2017-03-01 13:04:01 -0500

    print board

--- lib/ai/command/focus.rb
+++ lib/ai/command/focus.rb
@@ -7,6 +7,8 @@ class AI::Command::Focus < AI::Command::Base
   SMALL_SIZE = 4
   PLAYER_ONE = 0
   PLAYER_TWO = 1
+  EMPTY_SPACE = "x"
+  NULL_SPACE = nil
 
   attr_accessor :size, :board
 
@@ -20,8 +22,7 @@ class AI::Command::Focus < AI::Command::Base
 
       @board = Board.new(@size)
       @board.populate
-      puts @board.board.to_s
-
+      @board.print_state
     end
   end
 
@@ -36,13 +37,13 @@ class AI::Command::Focus < AI::Command::Base
 
       @board = Array.new(size/2)
       @board.map! do |arr|
-        Array.new(size)
+        Array.new(size, EMPTY_SPACE)
       end
-      @board.unshift(Array.new(size2)) if size2 > 0
-      @board.unshift(Array.new(size4)) if size4 > 0
+      @board.unshift(Array.new(size2, EMPTY_SPACE)) if size2 > 0
+      @board.unshift(Array.new(size4, EMPTY_SPACE)) if size4 > 0
 
-      @board.push(Array.new(size2)) if size2 > 0
-      @board.push(Array.new(size4)) if size4 > 0
+      @board.push(Array.new(size2, EMPTY_SPACE)) if size2 > 0
+      @board.push(Array.new(size4, EMPTY_SPACE)) if size4 > 0
     end
 
     def move(src, dest)
@@ -59,7 +60,7 @@ class AI::Command::Focus < AI::Command::Base
             player = (y % 2 == 0) ? PLAYER_ONE : PLAYER_TWO
             arr.map!.with_index do |space, x|
               if arr.size == DEFAULT_SIZE && (x == 0 || x == DEFAULT_SIZE - 1)
-                nil
+                EMPTY_SPACE
               else
                 if count > 1 && count % 2 == 0
                   player = if player == PLAYER_ONE
@@ -69,11 +70,36 @@ class AI::Command::Focus < AI::Command::Base
                   end
                 end
                 count += 1
-                player
+                [player]
               end
             end
           end
         end
+      end
+    end
+
+    def print_state
+      spacing = "%5s "
+      printf " %5s %5s %5s %5s %5s %5s %5s %5s\n", *(('A'..'H').to_a)
+      num = 1
+
+      board.each_with_index do |row, i|
+        formatting = ""
+        print num
+        num += 1
+
+        extra = (size - row.size) / 2
+        edges = []
+        extra.times do
+          edges.push(nil)
+        end
+
+        columns = row.size + extra
+        columns.times do
+          formatting += spacing
+        end
+        formatting += "\n"
+        printf formatting, *(edges + row)
       end
     end
   end
