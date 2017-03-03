968a387b524ee1b06e61f6080996fb8a65e110d1
Zachary Chai <zachary.chai@blindsidenetworks.com>
2017-03-03 17:49:51 -0500

    add ability to move

--- lib/ai/command/focus.rb
+++ lib/ai/command/focus.rb
@@ -7,10 +7,10 @@ class AI::Command::Focus < AI::Command::Base
   SMALL_SIZE = 4
   PLAYER_ONE = 0
   PLAYER_TWO = 1
-  EMPTY_SPACE = "x"
+  EMPTY_SPACE = []
   NULL_SPACE = nil
 
-  attr_accessor :size, :board
+  # attr_accessor :size, :board
 
   def index
     if @opts.help?
@@ -23,10 +23,14 @@ class AI::Command::Focus < AI::Command::Base
       @board = Board.new(@size)
       @board.populate
       @board.print_state
+      @board.move('b2', 'b3')
+      @board.print_state
     end
   end
 
   class Board
+
+    LETTERS = ('a'..'h').to_a
 
     attr_accessor :board, :size
 
@@ -47,7 +51,19 @@ class AI::Command::Focus < AI::Command::Base
     end
 
     def move(src, dest)
+      extra_pieces = 0
+      src_stack = position(src)
+      dest_stack = position(dest)
 
+      if pieces = src_stack.size + dest_stack.size > 5
+        extra_pieces = pieces - 5
+      end
+
+      new_stack = src_stack + dest_stack
+      set_position(dest, new_stack)
+      set_position(src, EMPTY_SPACE)
+
+      extra_pieces
     end
 
     def populate
@@ -79,8 +95,8 @@ class AI::Command::Focus < AI::Command::Base
     end
 
     def print_state
-      spacing = "%5s "
-      printf " %5s %5s %5s %5s %5s %5s %5s %5s\n", *(('A'..'H').to_a)
+      spacing = "%10s "
+      printf " %10s %10s %10s %10s %10s %10s %10s %10s\n", *(('A'..'H').to_a)
       num = 1
 
       board.each_with_index do |row, i|
@@ -101,6 +117,34 @@ class AI::Command::Focus < AI::Command::Base
         formatting += "\n"
         printf formatting, *(edges + row)
       end
+    end
+
+    def position(pos)
+      row = pos[1].to_i
+      column = letter_to_num(pos[0])
+      reduce = 0
+      if row == 1 || row == 8
+        reduce = 2
+      elsif row == 2 || row == 7
+        reduce = 1
+      end
+      board[row - 1][column - reduce]
+    end
+
+    def set_position(pos, value)
+      row = pos[1].to_i
+      column = letter_to_num(pos[0])
+      reduce = 0
+      if row == 1 || row == 8
+        reduce = 2
+      elsif row == 2 || row == 7
+        reduce = 1
+      end
+      board[row - 1][column - reduce] = value
+    end
+
+    def letter_to_num(letter)
+      LETTERS.index(letter.downcase)
     end
   end
 
